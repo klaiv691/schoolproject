90e794644c02b9500595f99cb3efed5745afafa8
klaiv691 <145677290+klaiv691@users.noreply.github.com>
2024-04-15 13:03:47 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,35 +1,60 @@
 from flask import Flask, jsonify
 import mysql.connector
+import logging
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-def fetch_largest_serial_number():
+logging.basicConfig(level=logging.DEBUG)
 
-    connection = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Ayesiga@123",
-        database="store"
-    )
+# Route for serving entrance data
+@app.route('/entrance')
+def get_entrance_data():
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="Ayesiga@123",
+            database="store"
+        )
 
+        cursor = connection.cursor()
+        cursor.execute("SELECT MAX(serial_number) FROM storage")
+        max_serial_number = cursor.fetchone()[0]
 
-    cursor = connection.cursor()
-    cursor.execute("SELECT MAX(serial_number) FROM storage")
-    largest_serial_number = cursor.fetchone()[0]
+        cursor.close()
+        connection.close()
 
+        return jsonify({"max_serial_number": max_serial_number})
+    except Exception as e:
+        # Log the exception
+        logging.error("An error occurred while fetching entrance data: %s", str(e))
+        return jsonify({"error": "An error occurred while fetching entrance data"}), 500
 
-    cursor.close()
-    connection.close()
+# Route for serving exit data
+@app.route('/exit')
+def get_exit_data():
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="Ayesiga@123",
+            database="store"
+        )
 
-    return largest_serial_number
+        cursor = connection.cursor()
+        cursor.execute("SELECT MAX(serial_number) FROM storage2")
+        max_serial_number = cursor.fetchone()[0]
 
-@app.route('/data')
-def get_data():
+        cursor.close()
+        connection.close()
 
-    largest_serial_number = fetch_largest_serial_number()
-
-
-    return jsonify({"largest_serial_number": largest_serial_number})
+        return jsonify({"max_serial_number": max_serial_number})
+    except Exception as e:
+        # Log the exception
+        logging.error("An error occurred while fetching exit data: %s", str(e))
+        return jsonify({"error": "An error occurred while fetching exit data"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5000)
